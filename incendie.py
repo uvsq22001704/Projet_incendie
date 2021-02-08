49ea93659e14b2e997272f29b3828ee4f1e72779
Hyacinthe MORASSE <hyacinthe.morasse@ens.uvsq.fr>
2021-02-08 15:22:27 +0100

    import des librairies créé

--- incendie.py
+++ incendie.py
@@ -1,1 +1,17 @@
-####
+######################
+# Groupe 2 LDDMP
+# Arthur CHAUVEAU
+# Noémie KAUFMANN
+# Titouan BIGET
+# Diary ANDRIANARIVO
+# Ibouroi MOHAMMED
+# Hyacinthe MORASSE
+# https://github.com/uvsq22001704/Projet_incendie
+######################
+
+######################
+#import des librairies
+
+import tkinter as tk
+
+######################
