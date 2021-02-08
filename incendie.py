24a818911ba7f3bb6ab7ea82ff99fa054f89b3d4
Hyacinthe MORASSE <hyacinthe.morasse@ens.uvsq.fr>
2021-02-08 16:22:03 +0100

    ok

--- incendie.py
+++ incendie.py
@@ -41,5 +41,12 @@ import tkinter as tk
 ######################
 # Programme principal
 
+canvas.grid(column=0, row=0, columnspan=3)
+button1.grid(column=0, row=1)
+button2.grid(column=1, row=1)
+button3.grid(column=2, row=1)
+button4.grid(column=0, row=2)
+button5.grid(column=1, row=2)
+button6.grid(column=2, row=2)
 
 ######################
