130388057f49c9146babbee416a3c57d3e08e6db
Hyacinthe <71659865+uvsq22003744@users.noreply.github.com>
2021-02-08 15:11:55 +0100

    Update incendie.py

--- incendie.py
+++ incendie.py
@@ -1,1 +1,11 @@
-####
+######################
+# Groupe 2 LDDMP
+# Arthur CHAUVEAU
+# Noémie KAUFMANN
+# Titouan BIGET
+# Diary ANDRIANARIVO
+# Ibouroi MOHAMMED
+# Hyacinthe MORASSE
+# https://github.com/uvsq22001704/Projet_incendie
+#######################
+
