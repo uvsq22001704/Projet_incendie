a133ade49009d752b3b1159adff2129179b5415c
Titouan BIGET <titouan.biget@uvsq.ens.fr>
2021-02-13 22:00:19 +0100

    uwu

---
+++ Zone de Test n°1.py
@@ -0,0 +1,71 @@
+######################
+
+
+#   Zone de Test n°1
+
+
+######################
+
+######################
+# Import des librairies
+
+import tkinter as tk
+import random as rd
+
+
+######################
+
+
+######################
+# Définition des constantes
+root = tk.Tk ()
+
+HEIGHT = 800
+WIDTH = 1600
+PARCEL_HEIGHT = HEIGHT / 20
+PARCEL_WIDTH = WIDTH / 40
+FIRE_DURATION = 0
+ASHES_DURATION = 0
+
+######################
+#j'essaye de faire un commit uwu
+
+######################
+# Défintion des variables globales
+
+COMPTEUR = 0
+
+######################
+
+
+######################
+# Défintion des fonctions
+
+
+######################
+
+
+######################
+# Programme principal
+
+canvas = tk.Canvas(root, width = WIDTH, height = HEIGHT, bg = "black")
+button1 = tk.Button(root, text = "TERRAIN ALÉATOIRE")
+button2 = tk.Button(root, text = "SAUVEGARDE DU TERRAIN")
+button3 = tk.Button(root, text = "CHARGER UN TERRAIN")
+button4 = tk.Button(root, text = "ÉTAPE DE SIMULATION")
+button5 = tk.Button(root, text = "DÉMARRER LA SIMULATION")
+button6 = tk.Button(root, text = "ARRÊTER LA SIMULATION")
+
+
+canvas.grid(column=0, row=0, columnspan=3)
+button1.grid(column=0, row=1)
+button2.grid(column=1, row=1)
+button3.grid(column=2, row=1)
+button4.grid(column=0, row=2)
+button5.grid(column=1, row=2)
+button6.grid(column=2, row=2)
+
+######################
+
+
+root.mainloop ()
